2536031af578084e4323d50ccde0a80ea69e9072
thandile <thandilexipu@gmail.com>
2016-11-05 16:48:20 +0200

    fixing files

---
+++ app/src/main/java/com/example/msf/msf/DataAdapter.java
@@ -0,0 +1,283 @@
+package com.example.msf.msf;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.example.msf.msf.Utils.WriteRead;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Thandile on 2016/11/03.
+ */
+
+public class DataAdapter {
+    public static String PATIENTINFOFILE = "Patients";
+    public static String ADVERSEINFOFILE = "AdverseEvents";
+    public static String USERINFOFILE = "Users";
+    public static String SESSIONTYPEFILE = "SessionType";
+    public static String PILOTINFOFILE = "Pilots";
+    public static String OUTCOMEFILE = "Outcomes";
+    public static String NOTIFICATIONFILE = "Notifications";
+    public static String MEDICALRECORDFILE = "MedicalRecords";
+
+
+    static String AD = "admission";
+    static String AE = "adverse event";
+    static String AP = "appointment";
+    static String CS = "counselling session";
+    static String MR = "medical report";
+    static String EV = "event";
+    static String PA = "patient";
+    static String PO = "patient outcome";
+    static String EN = "enroll";
+    static String RE = "regimen";
+
+
+    static String ADMISSIONS = "AD";
+    static String ADVERSE_EVENTS = "AE";
+    static String APPOINTMENTS = "AP";
+    static String COUNSELLING_SESSIONS = "CS";
+    static String MEDICAL_REPORTS = "MR";
+    static String EVENTS = "EV";
+    static String PATIENTS = "PA";
+    static String PATIENT_OUTCOMES = "PO";
+    static String ENROLLMENTS = "EN";
+    static String REGIMENS = "RE";
+
+    public static String getPatientInfo(Long pid, Context context) {
+        String patients = WriteRead.read(PATIENTINFOFILE, context);
+        String full_name = "";
+        try {
+            JSONArray jsonarray = new JSONArray(patients);
+            for (int i = 0; i < jsonarray.length(); i++) {
+                JSONObject jsonobject = jsonarray.getJSONObject(i);
+                if (jsonobject.getString("id").equals("" + pid)) {
+                    final String first_name = jsonobject.getString("other_names");
+                    String last_name = jsonobject.getString("last_name");
+                    full_name = first_name + " " + last_name;
+                    break;
+                }
+            }
+        } catch (JSONException e) {
+            System.out.print("unsuccessful");
+        }
+        return full_name;
+    }
+
+    public static String eventTypeGet(Long eventID, Context context) {
+        String events = WriteRead.read(ADVERSEINFOFILE, context);
+        String eventType = "";
+        try {
+            JSONArray jsonarray = new JSONArray(events);
+            for (int i = 0; i < jsonarray.length(); i++) {
+                JSONObject jsonobject = jsonarray.getJSONObject(i);
+                if (jsonobject.getString("id").equals("" + eventID)) {
+                    final String name = jsonobject.getString("name");
+                    eventType = name;
+                    break;
+                }
+            }
+        } catch (JSONException e) {
+            System.out.print("unsuccessful");
+        }
+        return eventType;
+    }
+
+    public static String usernames(Long uid, Context context) {
+        String user = "";
+        String users = WriteRead.read(USERINFOFILE, context);
+        try {
+            JSONArray jsonarray = new JSONArray(users);
+            for (int i = 0; i < jsonarray.length(); i++) {
+                JSONObject jsonObject = jsonarray.getJSONObject(i);
+                if (jsonObject.getString("id").equals("" + uid)) {
+                    int id = Integer.parseInt(jsonObject.getString("id"));
+                    String username = jsonObject.getString("username");
+                    user = username;
+                    break;
+                }
+            }
+        } catch (JSONException e) {
+            System.out.print("unsuccessful");
+        }
+        return user;
+    }
+
+
+    public static String sessionTypeLoad(Long sid, Context context) {
+        String session = "";
+        String sessionTypes = WriteRead.read(SESSIONTYPEFILE, context);
+        try {
+            JSONArray jsonarray = new JSONArray(sessionTypes);
+            for (int i = 0; i < jsonarray.length(); i++) {
+                JSONObject jsonobject = jsonarray.getJSONObject(i);
+                if (jsonobject.getString("id").equals("" + sid)) {
+                    String id_name = jsonobject.getString("id") + ": " + jsonobject.getString("name");
+                    session = id_name;
+                    break;
+                }
+            }
+        } catch (JSONException e) {
+            System.out.print("unsuccessful");
+        }
+        return session;
+    }
+
+    public static String loadPilots(Long pid, Context context){
+        String pilots = WriteRead.read(PILOTINFOFILE, context);
+        String pilot ="";
+        try {
+            JSONArray jsonarray = new JSONArray(pilots);
+            for (int i = 0; i < jsonarray.length(); i++) {
+                JSONObject jsonobject = jsonarray.getJSONObject(i);
+                if (jsonobject.getString("id").equals(""+pid)) {
+                    String id_name = jsonobject.getString("id") + ": " + jsonobject.getString("name");
+                    pilot = id_name;
+                }
+            }
+        }catch (JSONException e) {
+            System.out.print("unsuccessful");
+        }
+        return pilot;
+    }
+
+    public static String loadOutcomes(Long pid, Context context){
+        String pilots = WriteRead.read(OUTCOMEFILE, context);
+        String pilot ="";
+        try {
+            JSONArray jsonarray = new JSONArray(pilots);
+            for (int i = 0; i < jsonarray.length(); i++) {
+                JSONObject jsonobject = jsonarray.getJSONObject(i);
+                if (jsonobject.getString("id").equals(""+pid)) {
+                    String id_name = jsonobject.getString("id") + ": " + jsonobject.getString("name");
+                    pilot = id_name;
+                }
+            }
+        }catch (JSONException e) {
+            System.out.print("unsuccessful");
+        }
+        return pilot;
+    }
+
+    public static ArrayList<String> subscriptionID(Context context){
+        String user = "";
+        ArrayList<String> list = new ArrayList<String >();
+        String users = WriteRead.read(NOTIFICATIONFILE, context);
+        String [] abbrev = {PATIENTS, APPOINTMENTS, ENROLLMENTS, EVENTS, ADMISSIONS,
+                COUNSELLING_SESSIONS, ADVERSE_EVENTS, REGIMENS, MEDICAL_REPORTS, PATIENT_OUTCOMES};
+        try{
+            JSONArray jsonarray = new JSONArray(users);
+            for (int i = 0; i < jsonarray.length(); i++) {
+                JSONObject jsonObject = jsonarray.getJSONObject(i);
+                user = user + " "+ jsonObject.getString("service");
+                if (jsonObject.getString("service").equals(PATIENTS)) {
+                    list.add("patient");
+                }
+                if (jsonObject.getString("service").equals(APPOINTMENTS)) {
+                    list.add("appointment");
+                }
+                if (jsonObject.getString("service").equals(ENROLLMENTS)) {
+                    list.add("enrollment");
+                }
+                if (jsonObject.getString("service").equals(EVENTS)) {
+                    list.add("event");
+                }
+                if (jsonObject.getString("service").equals(ADMISSIONS)) {
+                    list.add("admission");
+                }
+                if (jsonObject.getString("service").equals(APPOINTMENTS)) {
+                    list.add("appointment");
+                }
+                if (jsonObject.getString("service").equals(COUNSELLING_SESSIONS)) {
+                    list.add("counselling");
+                }
+                if (jsonObject.getString("service").equals(ADVERSE_EVENTS)) {
+                    list.add("adverse event");
+                }
+                if (jsonObject.getString("service").equals(REGIMENS)) {
+                    list.add("regimen");
+                }
+                if (jsonObject.getString("service").equals(MEDICAL_REPORTS)) {
+                    list.add("medical");
+                }
+                if (jsonObject.getString("service").equals(PATIENT_OUTCOMES)) {
+                    list.add("outcome");
+                }
+            }
+        }
+        catch (JSONException e){
+            System.out.print("unsuccessful");
+        }
+        //Log.d(TAG, "username"+list.get(0));
+        return list;
+    }
+
+
+    public static String loadUserFromFile(Long uid, Context context){
+        String user = "";
+        String users = WriteRead.read(USERINFOFILE, context);
+        try{
+            JSONArray jsonarray = new JSONArray(users);
+            for (int i = 0; i < jsonarray.length(); i++) {
+                JSONObject jsonObject = jsonarray.getJSONObject(i);
+                if (jsonObject.getString("id").equals("" + uid)) {
+                    int id = Integer.parseInt(jsonObject.getString("id"));
+                    String username = jsonObject.getString("username");
+                    user =  username;
+                    break;
+                }
+            }
+        }
+        catch (JSONException e){
+            System.out.print("unsuccessful");
+        }
+        return user;
+    }
+
+    public static String getUserID(String username, Context context){
+        String user = "";
+        String users = WriteRead.read(USERINFOFILE, context);
+        try{
+            JSONArray jsonarray = new JSONArray(users);
+            for (int i = 0; i < jsonarray.length(); i++) {
+                JSONObject jsonObject = jsonarray.getJSONObject(i);
+                if (jsonObject.getString("username").equals(username)) {
+                    String id = jsonObject.getString("id");
+                    user =  id;
+                    break;
+                }
+            }
+        }
+        catch (JSONException e){
+            System.out.print("unsuccessful");
+        }
+        return user;
+    }
+
+    public static String loadRecordTypeFromFile(Long uid, Context context){
+        String user = "";
+        String users = WriteRead.read(MEDICALRECORDFILE, context);
+        try{
+            JSONArray jsonarray = new JSONArray(users);
+            for (int i = 0; i < jsonarray.length(); i++) {
+                JSONObject jsonObject = jsonarray.getJSONObject(i);
+                if (jsonObject.getString("id").equals("" + uid)) {
+                    int id = Integer.parseInt(jsonObject.getString("id"));
+                    String username = jsonObject.getString("username");
+                    user =  username;
+                    break;
+                }
+            }
+        }
+        catch (JSONException e){
+            System.out.print("unsuccessful");
+        }
+        return user;
+    }
+
+}
---
+++ app/src/main/java/com/example/msf/msf/Fragments/Appointment/AppointmentFragment.java
@@ -0,0 +1,270 @@
+package com.example.msf.msf.Fragments.Appointment;
+
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.amigold.fundapter.BindDictionary;
+import com.amigold.fundapter.FunDapter;
+import com.amigold.fundapter.extractors.StringExtractor;
+import com.example.msf.msf.API.Auth;
+import com.example.msf.msf.API.Models.Appointment;
+import com.example.msf.msf.API.ErrorEvent;
+import com.example.msf.msf.API.Interface;
+import com.example.msf.msf.API.ServerEvent;
+import com.example.msf.msf.DataAdapter;
+import com.example.msf.msf.HomeActivity;
+import com.example.msf.msf.LoginActivity;
+import com.example.msf.msf.Presenters.Admissions.IAdmissionListView;
+import com.example.msf.msf.Presenters.Appointments.AppointmentPresenter;
+import com.example.msf.msf.Presenters.Appointments.IAppointmentListView;
+import com.example.msf.msf.R;
+import com.example.msf.msf.Utils.AppStatus;
+import com.example.msf.msf.Utils.WriteRead;
+import com.squareup.otto.Subscribe;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.List;
+
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+import retrofit.mime.TypedByteArray;
+
+
+public class AppointmentFragment extends Fragment implements IAppointmentListView{
+    FloatingActionButton fab;
+    private final String TAG = this.getClass().getSimpleName();
+    ListView appointmentLV;
+    RadioButton all, own;
+    TextView text;
+    ProgressDialog prgDialog;
+    RadioGroup radioGroup;
+    DateFormat df = new SimpleDateFormat("dd/MM/yy HH:mm:ss");
+    Date dateobj = new Date();
+    AppointmentPresenter presenter;
+    static String appointments = "all";
+
+    public AppointmentFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        HomeActivity.navItemIndex = 3;
+        View view  = inflater.inflate(R.layout.fragment_appointment, container, false);
+        presenter = new AppointmentPresenter(this);
+        text = (TextView) view.findViewById(R.id.defaultText);
+        prgDialog = new ProgressDialog(AppointmentFragment.this.getActivity());
+        // Set Progress Dialog Text
+        prgDialog.setMessage("Please wait...");
+        // Set Cancelable as False
+        prgDialog.setCancelable(false);
+
+        all = (RadioButton) view.findViewById(R.id.allRadioButton);
+        own = (RadioButton) view.findViewById(R.id.ownRadioButton);
+        appointmentLV = (ListView) view.findViewById(R.id.appointmentLV);
+        listViewListener();
+        radioGroup = (RadioGroup) view.findViewById(R.id.radioGroup);
+        radioListener();
+        fab = (FloatingActionButton) view.findViewById(R.id.btnFloatingAction);
+        fabListener();
+        return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (AppStatus.getInstance(AppointmentFragment.this.getActivity()).isOnline()) {
+            presenter.loadAppointments();
+        } else {
+            text.setText("You are currently offline, therefore upcoming appointments cannot be loaded");
+        }
+    }
+
+    private void radioListener() {
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                AppointmentFragment appointmentFragment = new AppointmentFragment();
+                FragmentManager manager = getActivity().getSupportFragmentManager();
+                switch(checkedId) {
+                    case R.id.allRadioButton:
+                        appointments = "all";
+                        manager.beginTransaction()
+                                .replace(R.id.rel_layout_for_frag, appointmentFragment,
+                                        appointmentFragment.getTag())
+                                .addToBackStack(null)
+                                .commit();
+                        break;
+                    case R.id.ownRadioButton:
+                        appointments = "own";
+                        manager.beginTransaction()
+                                .replace(R.id.rel_layout_for_frag, appointmentFragment,
+                                        appointmentFragment.getTag())
+                                .addToBackStack(null)
+                                .commit();
+                        break;
+                }
+            }
+        });
+    }
+
+    private void fabListener() {
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CreateAppointmentFragment createAppointmentFragment = new CreateAppointmentFragment();
+                FragmentManager manager = getActivity().getSupportFragmentManager();
+                manager.beginTransaction()
+                        .replace(R.id.rel_layout_for_frag, createAppointmentFragment,
+                                createAppointmentFragment.getTag())
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+    }
+
+    private void listViewListener() {
+        appointmentLV.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                TextView idTV = (TextView) view.findViewById(R.id.idTV);
+                String id = idTV.getText().toString().split(" ")[1];
+                Log.e(TAG, id.toString());
+                AppointmentInfoFragment appointmentListFragment = new AppointmentInfoFragment().newInstance(id);
+                FragmentManager manager = getActivity().getSupportFragmentManager();
+
+                manager.beginTransaction()
+                        .replace(R.id.rel_layout_for_frag, appointmentListFragment,
+                                appointmentListFragment.getTag())
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+    }
+
+
+    @Override
+    public void onAppointmentLoadedSuccess(List<Appointment> list, Response response) {
+        text.setText("Upcoming appointments");
+        //ArrayList<Appointment> appointmentList = new ArrayList<Appointment>();
+        //appointmentList.addAll(list);
+        ArrayList<Appointment> appointmentToShow = new ArrayList<Appointment>();
+        for (int i = 0; i < list.size(); i++) {
+            if (df.format(dateobj).compareTo(list.get(i).getDate()) >= 0) {
+                if (appointments.equals("own")) {
+                    if (DataAdapter.usernames((long) list.get(i).getOwner(),
+                            getActivity()).equals(LoginActivity.username)) {
+                        appointmentToShow.add(list.get(i));
+                    }
+                }
+                else{
+                    appointmentToShow.addAll(list);
+                }
+            }
+        }
+        if (appointmentToShow.size()>0){
+            Collections.sort(appointmentToShow, new DateComparator());
+            Log.d(TAG, appointmentToShow.toString());
+            BindDictionary<Appointment> dictionary = new BindDictionary<>();
+            dictionary.addStringField(R.id.titleTV, new StringExtractor<Appointment>() {
+                @Override
+                public String getStringValue(Appointment appointment, int position) {
+                    return appointment.getTitle();
+                }
+            });
+            dictionary.addStringField(R.id.personTV, new StringExtractor<Appointment>() {
+                @Override
+                public String getStringValue(Appointment appointment, int position) {
+                    return "Owner: " + appointment.getOwnerName();
+                }
+            });
+
+            dictionary.addStringField(R.id.dateTV, new StringExtractor<Appointment>() {
+                @Override
+                public String getStringValue(Appointment appointment, int position) {
+                    return appointment.getDate();
+                }
+            });
+
+            dictionary.addStringField(R.id.idTV, new StringExtractor<Appointment>() {
+                @Override
+                public String getStringValue(Appointment appointment, int position) {
+                    return "ID: " + appointment.getId();
+                }
+            });
+            FunDapter adapter = new FunDapter(AppointmentFragment.this.getActivity(),
+                    appointmentToShow,
+                    R.layout.appointment_list_layout, dictionary);
+            appointmentLV.setAdapter(adapter);
+
+    }
+    else {
+
+            text.setText("No Scheduled appointments");
+            Toast.makeText(AppointmentFragment.this.getActivity(),
+                    "No Scheduled appointments", Toast.LENGTH_SHORT).show();
+            //appointmentList.add("No scheduled appointments.");
+        }
+    }
+
+    @Override
+    public void onAppointmentLoadedFailure(RetrofitError error) {
+        Toast.makeText(getActivity(), error.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+    }
+
+
+    public class DateComparator implements Comparator<Appointment>
+    {
+        public int compare(Appointment o1, Appointment o2)
+        {
+            return o1.getDate().compareTo(o2.getDate());
+        }
+    }
+
+    @Subscribe
+    public void onServerEvent(ServerEvent serverEvent){
+        prgDialog.hide();
+    }
+
+    @Subscribe
+    public void onErrorEvent(ErrorEvent errorEvent){
+        prgDialog.hide();
+        Toast.makeText(AppointmentFragment.this.getActivity(), "" +
+                errorEvent.getErrorMsg(), Toast.LENGTH_SHORT).show();
+        AppointmentFragment appointmentFragment = new AppointmentFragment();
+        FragmentManager manager = getActivity().getSupportFragmentManager();
+        manager.beginTransaction().replace(R.id.rel_layout_for_frag,
+                appointmentFragment,
+                appointmentFragment.getTag())
+                .addToBackStack(null)
+                .commit();
+
+    }
+}
